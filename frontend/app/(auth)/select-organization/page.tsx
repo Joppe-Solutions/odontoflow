3c807f66fca3e0b61cdfdd6dc0c526988c9d338d
phrodriguesilva <phrodriguesilva@hotmail.com>
2026-02-12 17:28:49 -0300

    vercel integration

--- frontend/app/(auth)/select-organization/page.tsx
+++ frontend/app/(auth)/select-organization/page.tsx
@@ -1,19 +1,26 @@
-import { CreateOrganization, OrganizationSwitcher } from "@clerk/nextjs";
+import { OrganizationList } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 /**
  * This page renders the Clerk CreateOrganization component.
  * See https://clerk.com/docs/components/organization/create-organization for more information.
  */
-export default function CreateOrganizationPage() {
+export default async function SelectOrganizationPage() {
+	const { orgId } = await auth();
+
+	if (orgId) {
+		redirect("/dashboard");
+	}
+
 	return (
 		<div className="min-h-screen flex flex-col gap-4 items-center justify-center">
 			<p>Select an organization to continue.</p>
 
-			<OrganizationSwitcher
+			<OrganizationList
 				afterSelectOrganizationUrl="/dashboard"
 				afterCreateOrganizationUrl="/dashboard"
 				hidePersonal
-				defaultOpen
 			/>
 		</div>
 	);
